0077dc8a614b92b3f7d3cbcfa8b0d913434b9896
Krzysztof Wilczynski <krzysztof.wilczynski@linux.com>
2016-06-13 21:03:55 +0900

    Change the way how reboot is handled, make it a deferred background task.
    Signed-off-by: Krzysztof Wilczynski <krzysztof.wilczynski@linux.com>

--- scripts/common/reboot.sh
+++ scripts/common/reboot.sh
@@ -24,6 +24,11 @@ export PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 
 # Make sure to shut the network interface down, thus close the
 # connections allowing for Packer to notice and reconnect.
+cat <<'EOF' > /tmp/reboot.sh
+sleep 10
+
+pgrep -f sshd | xargs kill -9
+
 if ifconfig &>/dev/null; then
     ifconfig eth0 down
     ifconfig eth0 up
@@ -32,5 +37,9 @@ else
     ip link set eth0 up
 fi
 
-shutdown -r now
+reboot -f
+EOF
+
+nohup bash /tmp/reboot.sh &>/dev/null &
+
 sleep 60
